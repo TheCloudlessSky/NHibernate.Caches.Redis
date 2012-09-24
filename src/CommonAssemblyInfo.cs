03f6e6d0255ff253d676cb5fecc107e52fe41b39
Adrian Phinney <adrian.phinney@bellaliant.net>
2012-09-23 22:48:25 -0300

    Build/Nuget configuration.

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyProduct("NHibernate.Caches.Redis")]
+
+[assembly: AssemblyCompany("http://www.adrianphinney.com")]
+[assembly: AssemblyCopyright("Copyright © Harbour Inc. 2012")]
+[assembly: AssemblyDescription("An NHibernate caching provider for Redis.")]
+
+[assembly: ComVisible(false)]
