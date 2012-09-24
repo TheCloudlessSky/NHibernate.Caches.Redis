c0fb90ed3d4696892167f23bc52410613664bfa9
Adrian Phinney <adrian.phinney@bellaliant.net>
2012-09-24 14:42:05 -0300

    Add CommonAssemblyInfo into build.

--- src/NHibernate.Caches.Redis/Properties/AssemblyInfo.cs
+++ src/NHibernate.Caches.Redis/Properties/AssemblyInfo.cs
@@ -3,15 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("NHibernate.Caches.Redis")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Harbour Inc.")]
 [assembly: AssemblyProduct("NHibernate.Caches.Redis")]
-[assembly: AssemblyCopyright("Copyright © Harbour Inc. 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("An NHibernate caching provider for Redis.")]
 [assembly: Guid("c3db6972-73e3-4a08-8a79-99e6c29d1d25")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("NHibernate.Caches.Redis.Tests")]
