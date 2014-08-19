33664059fa60f78069dc29c30cea3edc13c5ad6a
Adrian Phinney <adrian.phinney@gmail.com>
2014-08-19 09:47:56 -0400

    Remove AAA from tests.

--- tests/NHibernate.Caches.Redis.Tests/RedisCacheTests.cs
+++ tests/NHibernate.Caches.Redis.Tests/RedisCacheTests.cs
@@ -38,13 +38,11 @@ namespace NHibernate.Caches.Redis.Tests
         [Fact]
         void Put_should_serialize_item_and_set_with_expiry()
         {
-            // Arrange
-            var cache = new RedisCache("region", ConnectionMultiplexer, options);
+            var sut = new RedisCache("region", ConnectionMultiplexer, options);
 
-            // Act
-            cache.Put(999, new Person("Foo", 10));
-            // Assert
-            var cacheKey = cache.CacheNamespace.GetKey(999);
+            sut.Put(999, new Person("Foo", 10));
+
+            var cacheKey = sut.CacheNamespace.GetKey(999);
             var data = Redis.StringGet(cacheKey);
             var expiry = Redis.KeyTimeToLive(cacheKey);
 
@@ -59,16 +57,13 @@ namespace NHibernate.Caches.Redis.Tests
         [Fact]
         void Configure_region_expiration_from_config_element()
         {
-            // Arrange
             var configElement = new RedisCacheElement("region", TimeSpan.FromMinutes(99));
             var props = new Dictionary<string, string>();
-            var cache = new RedisCache("region", props, configElement, ConnectionMultiplexer, options);
+            var sut = new RedisCache("region", props, configElement, ConnectionMultiplexer, options);
 
-            // Act
-            cache.Put(999, new Person("Foo", 10));
+            sut.Put(999, new Person("Foo", 10));
 
-            // Assert
-            var cacheKey = cache.CacheNamespace.GetKey(999);
+            var cacheKey = sut.CacheNamespace.GetKey(999);
             var expiry = Redis.KeyTimeToLive(cacheKey);
             Assert.True(expiry >= TimeSpan.FromMinutes(98) && expiry <= TimeSpan.FromMinutes(99));
         }
@@ -76,18 +71,15 @@ namespace NHibernate.Caches.Redis.Tests
         [Fact]
         void Put_should_retry_until_generation_matches_the_server()
         {
-            // Arrange
-            var cache = new RedisCache("region", ConnectionMultiplexer, options);
+            var sut = new RedisCache("region", ConnectionMultiplexer, options);
 
             // Another client incremented the generation.
-            Redis.StringIncrement(cache.CacheNamespace.GetGenerationKey(), 100);
+            Redis.StringIncrement(sut.CacheNamespace.GetGenerationKey(), 100);
 
-            // Act
-            cache.Put(999, new Person("Foo", 10));
+            sut.Put(999, new Person("Foo", 10));
 
-            // Assert
-            Assert.Equal(cache.CacheNamespace.GetGeneration(), 101);
-            var data = Redis.StringGet(cache.CacheNamespace.GetKey(999));
+            Assert.Equal(sut.CacheNamespace.GetGeneration(), 101);
+            var data = Redis.StringGet(sut.CacheNamespace.GetKey(999));
             var person = (Person)options.Serializer.Deserialize(data);
             Assert.Equal("Foo", person.Name);
             Assert.Equal(10, person.Age);
@@ -96,14 +88,11 @@ namespace NHibernate.Caches.Redis.Tests
         [Fact]
         void Get_should_deserialize_data()
         {
-            // Arrange
-            var cache = new RedisCache("region", ConnectionMultiplexer, options);
-            cache.Put(999, new Person("Foo", 10));
+            var sut = new RedisCache("region", ConnectionMultiplexer, options);
+            sut.Put(999, new Person("Foo", 10));
 
-            // Act
-            var person = cache.Get(999) as Person;
+            var person = sut.Get(999) as Person;
 
-            // Assert
             Assert.NotNull(person);
             Assert.Equal("Foo", person.Name);
             Assert.Equal(10, person.Age);
@@ -112,32 +101,26 @@ namespace NHibernate.Caches.Redis.Tests
         [Fact]
         void Get_should_return_null_if_not_exists()
         {
-            // Arrange
-            var cache = new RedisCache("region", ConnectionMultiplexer, options);
+            var sut = new RedisCache("region", ConnectionMultiplexer, options);
 
-            // Act
-            var person = cache.Get(99999) as Person;
+            var person = sut.Get(99999) as Person;
 
-            // Assert
             Assert.Null(person);
         }
 
         [Fact]
         void Get_should_retry_until_generation_matches_the_server()
         {
-            // Arrange
-            var cache1 = new RedisCache("region", ConnectionMultiplexer, options);
+            var sut1 = new RedisCache("region", ConnectionMultiplexer, options);
 
             // Another client incremented the generation.
-            Redis.StringIncrement(cache1.CacheNamespace.GetGenerationKey(), 100);
-            var cache2 = new RedisCache("region", ConnectionMultiplexer, options);
-            cache2.Put(999, new Person("Foo", 10));
+            Redis.StringIncrement(sut1.CacheNamespace.GetGenerationKey(), 100);
+            var sut2 = new RedisCache("region", ConnectionMultiplexer, options);
+            sut2.Put(999, new Person("Foo", 10));
 
-            // Act
-            var person = cache1.Get(999) as Person;
+            var person = sut1.Get(999) as Person;
 
-            // Assert
-            Assert.Equal(101, cache1.CacheNamespace.GetGeneration());
+            Assert.Equal(101, sut1.CacheNamespace.GetGeneration());
             Assert.NotNull(person);
             Assert.Equal("Foo", person.Name);
             Assert.Equal(10, person.Age);
@@ -146,79 +129,66 @@ namespace NHibernate.Caches.Redis.Tests
         [Fact]
         void Put_and_Get_into_different_cache_regions()
         {
-            // Arrange
             const int key = 1;
-            var cache1 = new RedisCache("region_A", ConnectionMultiplexer, options);
-            var cache2 = new RedisCache("region_B", ConnectionMultiplexer, options);
+            var sut1 = new RedisCache("region_A", ConnectionMultiplexer, options);
+            var sut2 = new RedisCache("region_B", ConnectionMultiplexer, options);
 
-            // Act
-            cache1.Put(key, new Person("A", 1));
-            cache2.Put(key, new Person("B", 1));
+            sut1.Put(key, new Person("A", 1));
+            sut2.Put(key, new Person("B", 1));
 
-            // Assert
-            Assert.Equal("A", ((Person)cache1.Get(1)).Name);
-            Assert.Equal("B", ((Person)cache2.Get(1)).Name);
+            Assert.Equal("A", ((Person)sut1.Get(1)).Name);
+            Assert.Equal("B", ((Person)sut2.Get(1)).Name);
         }
 
         [Fact]
         void Remove_should_remove_from_cache()
         {
-            // Arrange
-            var cache = new RedisCache("region", ConnectionMultiplexer, options);
-            cache.Put(999, new Person("Foo", 10));
+            var sut = new RedisCache("region", ConnectionMultiplexer, options);
+            sut.Put(999, new Person("Foo", 10));
 
-            // Act
-            cache.Remove(999);
+            sut.Remove(999);
 
-            // Assert
-            var result = Redis.StringGet(cache.CacheNamespace.GetKey(999));
+            var result = Redis.StringGet(sut.CacheNamespace.GetKey(999));
             Assert.False(result.HasValue);
         }
 
         [Fact]
         void Remove_should_retry_until_generation_matches_the_server()
         {
-            // Arrange
-            var cache1 = new RedisCache("region", ConnectionMultiplexer, options);
+            var sut1 = new RedisCache("region", ConnectionMultiplexer, options);
 
             // Another client incremented the generation.
-            Redis.StringIncrement(cache1.CacheNamespace.GetGenerationKey(), 100);
-            var cache2 = new RedisCache("region", ConnectionMultiplexer, options);
-            cache2.Put(999, new Person("Foo", 10));
+            Redis.StringIncrement(sut1.CacheNamespace.GetGenerationKey(), 100);
+            var sut2 = new RedisCache("region", ConnectionMultiplexer, options);
+            sut2.Put(999, new Person("Foo", 10));
 
-            // Act
-            cache1.Remove(999);
+            sut1.Remove(999);
 
-            // Assert
-            Assert.Equal(101, cache1.CacheNamespace.GetGeneration());
-            var result = Redis.StringGet(cache1.CacheNamespace.GetKey(999));
+            Assert.Equal(101, sut1.CacheNamespace.GetGeneration());
+            var result = Redis.StringGet(sut1.CacheNamespace.GetKey(999));
             Assert.False(result.HasValue);
         }
 
         [Fact]
         void Clear_update_generation_and_clear_keys_for_this_region()
         {
-            // Arrange
-            var cache = new RedisCache("region", ConnectionMultiplexer, options);
-            cache.Put(1, new Person("Foo", 1));
-            cache.Put(2, new Person("Bar", 2));
-            cache.Put(3, new Person("Baz", 3));
-            var oldKey1 = cache.CacheNamespace.GetKey(1);
-            var oldKey2 = cache.CacheNamespace.GetKey(2);
-            var oldKey3 = cache.CacheNamespace.GetKey(3);
+            var sut = new RedisCache("region", ConnectionMultiplexer, options);
+            sut.Put(1, new Person("Foo", 1));
+            sut.Put(2, new Person("Bar", 2));
+            sut.Put(3, new Person("Baz", 3));
+            var oldKey1 = sut.CacheNamespace.GetKey(1);
+            var oldKey2 = sut.CacheNamespace.GetKey(2);
+            var oldKey3 = sut.CacheNamespace.GetKey(3);
 
-            var setOfKeysKey = cache.CacheNamespace.GetSetOfKeysKey();
+            var setOfKeysKey = sut.CacheNamespace.GetSetOfKeysKey();
 
-            // Act
-            cache.Clear();
+            sut.Clear();
 
-            // Assert
-            
             // New generation.
-            Assert.Equal(2, cache.CacheNamespace.GetGeneration());
-            Assert.False(Redis.StringGet(cache.CacheNamespace.GetKey(1)).HasValue);
-            Assert.False(Redis.StringGet(cache.CacheNamespace.GetKey(2)).HasValue);
-            Assert.False(Redis.StringGet(cache.CacheNamespace.GetKey(3)).HasValue);
+            Assert.Equal(2, sut.CacheNamespace.GetGeneration());
+            Assert.False(Redis.StringGet(sut.CacheNamespace.GetKey(1)).HasValue);
+            Assert.False(Redis.StringGet(sut.CacheNamespace.GetKey(2)).HasValue);
+            Assert.False(Redis.StringGet(sut.CacheNamespace.GetKey(3)).HasValue);
             
             // List of keys for this region was cleared.
             Assert.False(Redis.StringGet(setOfKeysKey).HasValue);
@@ -235,63 +205,54 @@ namespace NHibernate.Caches.Redis.Tests
         [Fact]
         void Clear_should_ensure_generation_if_another_cache_has_already_incremented_the_generation()
         {
-            // Arrange
-            var cache = new RedisCache("region", ConnectionMultiplexer, options);
+            var sut = new RedisCache("region", ConnectionMultiplexer, options);
 
             // Another cache updated its generation (by clearing).
-            Redis.StringIncrement(cache.CacheNamespace.GetGenerationKey(), 100);
+            Redis.StringIncrement(sut.CacheNamespace.GetGenerationKey(), 100);
 
-            // Act
-            cache.Clear();
+            sut.Clear();
 
-            // Assert
-            Assert.Equal(102, cache.CacheNamespace.GetGeneration());
+            Assert.Equal(102, sut.CacheNamespace.GetGeneration());
         }
 
         [Fact]
         void Destroy_should_not_clear()
         {
-            // Arrange
-            var cache = new RedisCache("region", ConnectionMultiplexer, options);
+            var sut = new RedisCache("region", ConnectionMultiplexer, options);
 
-            // Act
-            cache.Destroy();
+            sut.Destroy();
 
-            // Assert
-            Assert.Equal(1, cache.CacheNamespace.GetGeneration());
+            Assert.Equal(1, sut.CacheNamespace.GetGeneration());
         }
 
         [Fact]
         void Lock_and_Unlock_concurrently_with_same_cache_client()
         {
-            // Arrange
-            var cache = new RedisCache("region", ConnectionMultiplexer, options);
-            cache.Put(1, new Person("Foo", 1));
+            var sut = new RedisCache("region", ConnectionMultiplexer, options);
+            sut.Put(1, new Person("Foo", 1));
 
             var results = new ConcurrentQueue<string>();
             const int numberOfClients = 5;
 
-            // Act
             var tasks = new List<Task>();
             for (var i = 1; i <= numberOfClients; i++)
             {
                 int clientNumber = i;
                 var t = Task.Factory.StartNew(() =>
                 {
-                    cache.Lock(1);
+                    sut.Lock(1);
                     results.Enqueue(clientNumber + " lock");
 
                     // Atrifical concurrency.
                     Thread.Sleep(100);
 
                     results.Enqueue(clientNumber + " unlock");
-                    cache.Unlock(1);
+                    sut.Unlock(1);
                 });
 
                 tasks.Add(t);
             }
 
-            // Assert
             Task.WaitAll(tasks.ToArray());
 
             // Each Lock should be followed by its associated Unlock.
@@ -307,14 +268,12 @@ namespace NHibernate.Caches.Redis.Tests
         [Fact]
         void Lock_and_Unlock_concurrently_with_different_cache_clients()
         {
-            // Arrange
             var mainCache = new RedisCache("region", ConnectionMultiplexer, options);
             mainCache.Put(1, new Person("Foo", 1));
 
             var results = new ConcurrentQueue<string>();
             const int numberOfClients = 5;
 
-            // Act
             var tasks = new List<Task>();
             for (var i = 1; i <= numberOfClients; i++)
             {
@@ -335,7 +294,6 @@ namespace NHibernate.Caches.Redis.Tests
                 tasks.Add(t);
             }
 
-            // Assert
             Task.WaitAll(tasks.ToArray());
 
             // Each Lock should be followed by its associated Unlock.
@@ -353,15 +311,12 @@ namespace NHibernate.Caches.Redis.Tests
         {
             using (var invalidConnectionMultiplexer = ConnectionMultiplexer.Connect(InvalidHost))
             {
-                // Arrange
                 const int key = 1;
-                var cache = new RedisCache("region_A", invalidConnectionMultiplexer, options);
+                var sut = new RedisCache("region_A", invalidConnectionMultiplexer, options);
 
-                // Act
-                cache.Put(key, new Person("A", 1));
+                sut.Put(key, new Person("A", 1));
 
-                // Assert
-                Assert.Null(cache.Get(key));
+                Assert.Null(sut.Get(key));
             }
         }
 
@@ -370,16 +325,14 @@ namespace NHibernate.Caches.Redis.Tests
         {
             using (var invalidConnectionMultiplexer = ConnectionMultiplexer.Connect(InvalidHost))
             {
-                // Arrange
                 const int key = 1;
-                var cache = new RedisCache("region_A", invalidConnectionMultiplexer, options);
+                var sut = new RedisCache("region_A", invalidConnectionMultiplexer, options);
 
-                // Act / Assert
                 Assert.DoesNotThrow(() =>
                 {
-                    cache.Put(key, new Person("A", 1));
-                    cache.Lock(key);
-                    cache.Unlock(key);
+                    sut.Put(key, new Person("A", 1));
+                    sut.Lock(key);
+                    sut.Unlock(key);
                 });
             }
         }
@@ -389,14 +342,12 @@ namespace NHibernate.Caches.Redis.Tests
         {
             using (var invalidConnectionMultiplexer = ConnectionMultiplexer.Connect(InvalidHost))
             {
-                // Arrange
                 const int key = 1;
-                var cache = new RedisCache("region_A", invalidConnectionMultiplexer, options);
+                var sut = new RedisCache("region_A", invalidConnectionMultiplexer, options);
 
-                // Act
                 Assert.DoesNotThrow(() =>
                 {
-                    cache.Remove(key);
+                    sut.Remove(key);
                 });
             }
         }
@@ -404,18 +355,15 @@ namespace NHibernate.Caches.Redis.Tests
         [Fact]
         void Should_update_server_generation_when_server_has_less_generation_than_the_client()
         {
-            // Arrange
             const int key = 1;
-            var cache = new RedisCache("region", ConnectionMultiplexer, options);
+            var sut = new RedisCache("region", ConnectionMultiplexer, options);
 
-            // Act
-            cache.Put(key, new Person("A", 1));
+            sut.Put(key, new Person("A", 1));
             FlushDb();
-            cache.Put(key, new Person("B", 2));
+            sut.Put(key, new Person("B", 2));
 
-            // Assert
-            var generationKey = cache.CacheNamespace.GetGenerationKey();
-            Assert.Equal(cache.CacheNamespace.GetGeneration().ToString(CultureInfo.InvariantCulture), (string)Redis.StringGet(generationKey));
+            var generationKey = sut.CacheNamespace.GetGenerationKey();
+            Assert.Equal(sut.CacheNamespace.GetGeneration().ToString(CultureInfo.InvariantCulture), (string)Redis.StringGet(generationKey));
         }
     }
 }
