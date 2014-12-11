b2df6e685ee43e4f4cc0eb52b7754ab75cf663d3
Adrian Phinney <adrian.phinney@gmail.com>
2014-12-11 15:34:35 -0500

    Add test for concurrent session factories.

--- tests/NHibernate.Caches.Redis.Tests/PerformanceTests.cs
+++ tests/NHibernate.Caches.Redis.Tests/PerformanceTests.cs
@@ -10,7 +10,7 @@ namespace NHibernate.Caches.Redis.Tests
     public class PerformanceTests : IntegrationTestBase
     {
         [Fact]
-        async Task concurrent_reads_and_writes()
+        async Task concurrent_sessions_with_reads_and_writes()
         {
             DisableLogging();
 
@@ -33,6 +33,46 @@ namespace NHibernate.Caches.Redis.Tests
                         entity.Name = Guid.NewGuid().ToString();
                         session.Flush();
                     });
+                });
+            });
+
+            await Task.WhenAll(tasks);
+        }
+
+        [Fact]
+        async Task concurrent_session_factories_with_reads_and_writes()
+        {
+            DisableLogging();
+
+            const int sessionFactoryCount = 5;
+            const int iterations = 1000;
+
+            // Create factories on the same thread so we don't run into
+            // concurrency issues with NHibernate.
+            var sessionFactories = Enumerable.Range(0, sessionFactoryCount).Select(i =>
+            {
+                return CreateSessionFactory();
+            });
+
+            var tasks = sessionFactories.Select(sessionFactory =>
+            {
+                return Task.Run(() =>
+                {
+                    for (int i = 0; i < iterations; i++)
+                    {
+                        object entityId = null;
+                        UsingSession(sessionFactory, session =>
+                        {
+                            var entity = new Person("Foo", 1);
+                            entityId = session.Save(entity);
+                            session.Flush();
+                            session.Clear();
+
+                            entity = session.Load<Person>(entityId);
+                            entity.Name = Guid.NewGuid().ToString();
+                            session.Flush();
+                        });
+                    }
                 });
             });
 
