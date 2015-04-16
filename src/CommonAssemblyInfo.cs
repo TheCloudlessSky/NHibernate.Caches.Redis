19977883183cbf493648b0749ace195e32cacafe
Adrian Phinney <adrian.phinney@gmail.com>
2015-04-16 15:15:06 -0400

    Bump to 2.0.0.

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: AssemblyCompany("http://adrianphinney.com")]
-[assembly: AssemblyCopyright("Copyright © Harbour Inc. 2014")]
+[assembly: AssemblyCopyright("Copyright © Harbour Inc. 2015")]
 [assembly: ComVisible(false)]
